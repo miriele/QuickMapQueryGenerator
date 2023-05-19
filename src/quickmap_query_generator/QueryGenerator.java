bf68816e55ba8b7bea2a0124844b6c5f0666dfb6
박진우 <miriele75@gmail.com>
2023-05-19 17:11:05 +0900

    symptom generator 추가

--- src/quickmap_query_generator/QueryGenerator.java
+++ src/quickmap_query_generator/QueryGenerator.java
@@ -17,6 +17,9 @@ public class QueryGenerator {
 		QueryGeneratorService	service	= new QueryGeneratorService();
 		service.generate(path + "data_qm_service.txt", path + "12_service.sql", nSep);
 		
+		QueryGeneratorSymptom	symptom	= new QueryGeneratorSymptom();
+		symptom.generate(path + "data_qm_symptom.txt", path + "13_symptom.sql", nSep);
+		
 		QueryGeneratorHos	genHos	= new QueryGeneratorHos();
 		genHos.generate(path + "data_qm_office_hospital.txt", path + "data_qm_service.txt", path + "20_office_hos.sql", nSep);
 		
